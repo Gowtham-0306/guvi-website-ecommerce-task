bf8c6fabbcf2f35a6e2e8c9ce5f5297762f31dea
Gowtham-0306 <141541254+Gowtham-0306@users.noreply.github.com>
2024-03-05 00:47:14 +0530

    Update productscard.jsx

--- src/components/productscard.jsx
+++ src/components/productscard.jsx
@@ -14,16 +14,16 @@ export default function Productscard( { handlecart = () =>{} , name= String  , p
 
   <div className="col mb-3">
       <div className="card h-100">
-          {/* Sale badge*/}
+          {/* */}
           <div className="badge bg-dark text-white position-absolute" style={{ top: "0.5rem", right: "0.5rem" }}>Sale</div>
-          {/* Product image*/}
+          {/* */}
           <img className="card-img-top" src={image} alt="..." />
-          {/* Product details*/}
+          {/* */}
           <div className="card-body p-4">
               <div className="text-center">
-                  {/* Product name*/}
+                  {/* */}
                   <h5 className="fw-bolder">{name}</h5>
-                  {/* Product reviews*/}
+                  {/* */}
                   <div className="d-flex justify-content-center small text-warning mb-2">
                       <div className="bi-star-fill"></div>
                       <div className="bi-star-fill"></div>
@@ -59,7 +59,7 @@ export default function Productscard( { handlecart = () =>{} , name= String  , p
           </div>
       </div>
   </div>
-  {/* You can add more card elements here if needed */}
+  {/* */}
 </>
 
    
